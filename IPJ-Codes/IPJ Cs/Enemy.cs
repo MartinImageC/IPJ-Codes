dc5b242c27e47eb8df5de2de0d45351d0291318b
Martin Lopez <martinlg2907@gmail.com>
2021-06-15 20:23:39 -0300

    Added Game

---
+++ IPJ-Codes/IPJ Cs/Enemy.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+public class Enemy
+{
+	private string name;
+	private int maxLife;
+	private int life;
+	private int maxMana;
+	private int mana;
+
+	private int MinAttack;
+	private int MaxAttack;
+
+	public Enemy() 
+	{
+	}
+	public Enemy(string name, int maxLife, int maxMana, int minAttack, int maxAttack)
+	{
+		this.name = name;
+		this.maxLife = maxLife;
+		this.life = maxLife;
+		this.maxMana = maxMana;
+		this.mana = maxMana;
+		this.MinAttack = minAttack;
+		this.MaxAttack = maxAttack;
+	}
+
+	public BinaryWriter Save(BinaryWriter bw) 
+	{
+		bw.Write(name);
+		bw.Write(maxLife);
+		bw.Write(life);
+		bw.Write(maxMana);
+		bw.Write(mana);
+		bw.Write(MinAttack);
+		bw.Write(MaxAttack);
+		return bw;
+	}
+
+	public BinaryReader Load(BinaryReader br) 
+	{
+		name = br.ReadString();
+		maxLife = br.ReadInt32();
+		life = br.ReadInt32();
+		maxMana = br.ReadInt32();
+		mana = br.ReadInt32();
+		MinAttack = br.ReadInt32();
+		MaxAttack = br.ReadInt32();
+		return br;
+	}
+
+	public Player Attack(Player player)
+	{
+		Random random = new Random();
+		int damage = random.Next(MinAttack, MaxAttack);
+		player.DoDamage(damage);
+		return player;
+	}
+
+	public string GetStatus()
+	{
+		string status = "";
+
+		status += "Name: " + name + "\n";
+		status += "Life: " + life + "\n";
+
+		return status;
+	}
+
+	public void DoDamage(int amount)
+	{
+		life -= amount;
+		if (life <= 0)
+		{
+			Console.WriteLine("F " + name);
+		}
+	}
+
+	public bool IsDead() 
+	{
+		return life <= 0;
+	}
+
+}
---
+++ IPJ-Codes/IPJ Cs/Floor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+class Floor
+{
+	public List<Enemy> enemies;
+	public Floor(int maxEnemeiesInFloor)
+	{
+		enemies = new List<Enemy>();
+		Random random = new Random();
+		int enemiesInFloor = random.Next(1, maxEnemeiesInFloor);
+		for (int i = 0; i < enemiesInFloor; i++)
+		{
+			enemies.Add(new Enemy("Beholder", 1000, 50, 20, 50));
+		}
+	}
+	public Floor() 
+	{
+
+	}
+
+	public BinaryWriter Save(BinaryWriter bw) 
+	{
+		bw.Write(enemies.Count);
+		for (int i = 0; i < enemies.Count; i++)
+		{
+			bw = enemies[i].Save(bw);
+		}
+		return bw;
+	}
+
+	public BinaryReader Load(BinaryReader br) 
+	{
+		int enemeiesCount = br.ReadInt32();
+		enemies = new List<Enemy>();
+		for (int i = 0; i < enemeiesCount; i++)
+		{
+			Enemy enemy = new Enemy();
+			br = enemy.Load(br);
+			enemies.Add(enemy);
+		}
+		return br;
+	}
+
+
+	public Player Fight(Player player)
+	{
+		for (int i = 0; i < enemies.Count; i++)
+		{
+			player = enemies[i].Attack(player);
+		}
+
+		enemies = player.Attack(enemies);
+
+		List<Enemy> enemiesAlive = new List<Enemy>();
+		for (int i = 0; i < enemies.Count; i++)
+		{
+			if (enemies[i].IsDead() == false)
+			{
+				enemiesAlive.Add(enemies[i]);
+			}
+		}
+		enemies = enemiesAlive;
+		return player;
+	}
+}
