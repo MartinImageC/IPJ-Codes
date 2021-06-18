db110b33b892edc207d327568c501e448be81366
Martin Lopez <martinlg2907@gmail.com>
2021-06-18 14:57:20 -0300

    Added Boss & Final Stage

---
+++ IPJ-Codes/IPJ Cs/Balcony.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class Balcony
+{
+    enum Options { Fight, Leave, Error }
+
+    private Boss boss = new Boss("Gygas", 200, 999, 35, 80);
+    private Options option;
+    public Balcony()
+    {
+
+    }
+    public Player InsideBalcony(Player player)
+    {
+        Console.WriteLine("You have reached The Balcony");
+        Console.WriteLine("And now The Ultimate Challenge");
+        boss.GetStatus();
+        Console.WriteLine("Leave = 0    -    Fight = 1");
+        switch (option)
+        {
+            case Options.Fight:
+                player = boss.Fight(player);
+                break;
+            case Options.Leave:
+                player = Leave(player);
+                break;
+        }
+        return player;
+    }
+
+    public Player Leave(Player player)
+    {
+        return player;
+    }
+}
---
+++ IPJ-Codes/IPJ Cs/Boss.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Boss
+{
+	private string name;
+	private int maxLife;
+	private int life;
+	private int maxMana;
+	private int mana;
+
+	private int MinAttack;
+	private int MaxAttack;
+
+	public Boss(string name, int maxLife, int maxMana, int minAttack, int maxAttack)
+	{
+		this.name = name;
+		this.maxLife = maxLife;
+		this.life = maxLife;
+		this.maxMana = maxMana;
+		this.mana = maxMana;
+		this.MinAttack = minAttack;
+		this.MaxAttack = maxAttack;
+	}
+
+	public Player Fight(Player player)
+	{
+		do
+		{
+			Console.WriteLine("Attack = 1     -    Defend = 2");
+			int answer = Convert.ToInt32(Console.ReadLine());
+			switch (answer)
+			{
+				case 1:
+					DoDamage(20);
+					break;
+				case 2:
+					Attack(player);
+					break;
+			}
+		} while (maxLife > 0);
+		return player;
+	}
+	public Player Attack(Player player)
+	{
+		Random random = new Random();
+		int damage = random.Next(MinAttack, MaxAttack);
+		player.DoDamage(damage);
+		return player;
+	}
+	public string GetStatus()
+	{
+		string status = "";
+
+		status += "Name: " + name + "\n";
+		status += "Life: " + life + "\n";
+
+		return status;
+	}
+	public void DoDamage(int amount)
+	{
+		life -= amount;
+		if (life <= 0)
+		{
+			Console.WriteLine("F " + name);
+		}
+	}
+	public bool IsDead()
+	{
+		return life <= 0;
+	}
+
+}
+
